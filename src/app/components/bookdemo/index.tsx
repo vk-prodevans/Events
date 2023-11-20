46221aacec40b538c0b324ca656114e35365301d
Rani4221 <mishrasriyarani21@gmail.com>
2023-11-20 17:40:28 +0530

    Add field with plus button and validation of form

--- src/app/components/bookdemo/index.tsx
+++ src/app/components/bookdemo/index.tsx
@@ -1,77 +1,124 @@
-import React from 'react'
-import { useState } from 'react';
-// import './Styles/Demo.css'
+import React, { useState } from 'react';
 import UploadFile from '../uploadFile';
 import TechVersionDropdown from '../techVersionDropdown';
-import '../../styles/Demo.css'
+import '../../styles/Demo.css';
 
 const BookDemo = () => {
   const [formCounter, setFormCounter] = useState(1);
-  const [inputValue, setInputValue] = useState<string>('');
-  const [isValid, setIsValid] = useState<boolean>(true);
-   const [formData, setFormData] = useState({
-    names: ['', '', ''],
-    emails: ['', '', ''],
+  const [formData, setFormData] = useState({
+    names: [''],
+    emails: [''],
     topic: '',
     description: '',
     techStack: '',
-   });
-   const addFormGroup = () => {
+  });
+
+  const [nameErrors, setNameErrors] = useState(['']);
+
+  const addFormGroup = () => {
     setFormCounter(formCounter + 1);
-  };
-  const removeFormGroup = (index:number) => {
-    
-    const updatedFormGroups = [...Array(formCounter)].filter((_, i) => i !== index);
-    setFormCounter(updatedFormGroups.length);
-  };
-
-  // const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-  //   const updatedNames = [...formData.names];
-  //   updatedNames[index] = e.target.value;
-  //   setFormData({ ...formData, names: updatedNames });
-  // };
-
-  
-
- const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue: string = event.target.value;
-     setInputValue(newValue);
-  
-
-    const isValidInput: boolean = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(newValue);
-    setIsValid(isValidInput);
-  };
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue: string = event.target.value;
-    setInputValue(newValue);
-    const isValidInput: boolean = /^[a-zA-Z\s]+$/.test(newValue);
-    setIsValid(isValidInput);
+    setFormData({
+      ...formData,
+      names: [...formData.names, ''],
+      emails: [...formData.emails, ''],
+    });
+    setNameErrors([...nameErrors, '']);
+    document.querySelector('.contactForm')?.classList.add('expanded');
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLSelectElement>) => {
+  const removeFormGroup = (index) => {
+    const updatedNames = [...formData.names];
+    const updatedEmails = [...formData.emails];
+    const updatedNameErrors = [...nameErrors];
+
+    updatedNames.splice(index, 1);
+    updatedEmails.splice(index, 1);
+    updatedNameErrors.splice(index, 1);
+
+    setFormCounter(updatedNames.length);
+    setFormData({ ...formData, names: updatedNames, emails: updatedEmails });
+    setNameErrors(updatedNameErrors);
+  };
+
+  const handleNameChange = (e, index) => {
+    const updatedNames = [...formData.names];
+    const inputValue = e.target.value;
+
+    // Validate that the input contains only letters
+    if (/^[A-Za-z]+$/.test(inputValue) || inputValue === '') {
+      updatedNames[index] = inputValue;
+      setFormData({ ...formData, names: updatedNames });
+      // Clear the error if validation passes
+      setNameErrors((prevErrors) => {
+        const errors = [...prevErrors];
+        errors[index] = '';
+        return errors;
+      });
+    } else {
+      // Set the error if validation fails
+      setNameErrors((prevErrors) => {
+        const errors = [...prevErrors];
+        errors[index] = 'Please enter only letters';
+        return errors;
+      });
+    }
+  };
+
+  const handleEmailChange = (e, index) => {
+    const updatedEmails = [...formData.emails];
+    updatedEmails[index] = e.target.value;
+    setFormData({ ...formData, emails: updatedEmails });
+  };
+
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission here
+    // Add form submission logic here
+
+    // Check for errors before submitting
+    const hasErrors = nameErrors.some((error) => error !== '');
+    if (hasErrors) {
+      // Handle errors, e.g., display a message or prevent form submission
+      console.log('Form has errors. Please correct them before submitting.');
+    } else {
+      // Form submission logic
+      console.log('Form submitted successfully:', formData);
+    }
   };
 
   return (
     <form className="contactForm" onSubmit={handleSubmit}>
-       {[...Array(formCounter)].map((_, index) => (
-          <div className="form-group" key={`group${index + 1}`}>
-            <label htmlFor={`name${index + 1}`}>Name</label>
-           <input type="text" id={`name${index + 1}`} pattern="^[a-zA-Z\s]+$" onChange={handleInput} placeholder="Enter name" />
-            {!isValid && <p style={{ color: 'red' }}>Please Enter Vlid name</p>}
-            <label htmlFor={`email${index + 1}`}>Email</label>
-           <input type="text" id={`email${index + 1}`} placeholder="Enter email" onChange={handleEmailChange} />
-            {!isValid && <p style={{ color: 'red' }}>Please Enter Vlid email</p>}
-            <button type="button" className='add-btn' onClick={addFormGroup}>+</button>
-            {formCounter > 1 && <button type="button" className='remove-btn' onClick={() => removeFormGroup(index)}>-</button>}
-          </div>
-        ))}
+      {formData.names.map((name, index) => (
+        <div key={index} className='inputGroup'>
+          <input
+            type="text"
+            className={`inputField ${nameErrors[index] ? 'error' : ''}`}
+            placeholder={`Name ${index + 1}`}
+            value={name}
+            onChange={(e) => handleNameChange(e, index)}
+          />
+          <span className="errorText">{nameErrors[index]}</span>
+          <input
+            type="email"
+            className="inputField"
+            placeholder={`Email ${index + 1}`}
+            value={formData.emails[index]}
+            onChange={(e) => handleEmailChange(e, index)}
+          />
+          <button type="button" className="add-btn" onClick={addFormGroup}>
+            +
+          </button>
+          {formCounter > 1 && (
+            <button type="button" className="remove-btn" onClick={() => removeFormGroup(index)}>
+              -
+            </button>
+          )}
+        </div>
+      ))}
       <textarea
         name="topic"
         className="textareaField"
@@ -86,25 +133,12 @@ const BookDemo = () => {
         value={formData.description}
         onChange={handleInputChange}
       />
-      {/* <select
-        name="techStack"
-        className='selectField'
-        value={formData.techStack}
-        onChange={handleInputChange}
-      >
-        <option value="">Select Tech Stack</option>
-        <option value="React">React</option>
-        <option value="Node.js">Node.js</option>
-        <option value="Python">Python</option>
-       
-          </select> */}
-      <TechVersionDropdown></TechVersionDropdown>
-          <label htmlFor="">File</label>
-          <UploadFile></UploadFile>
+      <TechVersionDropdown />
+      <label htmlFor="">File</label>
+      <UploadFile />
       <button type="submit" className='submitButton'>Submit</button>
     </form>
+  );
+};
 
-  )
-}
-
-export default BookDemo
+export default BookDemo;
