3b7fd34f8e2faa5f4f3f0a2d0044bf4a7fc8b88d
supriya-nune <supriya.nune@prodevans.com>
2023-11-21 17:54:41 +0530

    all demo modified and corrected with css

--- src/app/components/alldemo/index.tsx
+++ src/app/components/alldemo/index.tsx
@@ -1,34 +1,50 @@
-import React from 'react';
-import '../../styles/AllDemo.css'
+import React, { useState, useEffect } from "react";
+import "../../styles/AllDemo.css";
 
-const AllDemo: React.FC = () => {
+const AllDemo: React.FC =  () => {
+  const [alldemo, setAlldemo] = useState([]);
+  const [isDropdownActive, setDropdownActive] = useState(false);
+
   const toggleDropdown = () => {
-    const dropdown = document.querySelector('.dropdown') as HTMLElement | null;
-
-    if (dropdown) {
-      dropdown.classList.toggle('active');
-
-      const listData = document.querySelector('.listData') as HTMLElement | null;
-
-      if (listData) {
-        listData.style.display = listData.style.display === 'none' ? 'flex' : 'none';
-      }
-    }
+    setDropdownActive(!isDropdownActive);
   };
 
+ async  function getDetails() {
+    const dataFetch = async () => {
+      const resp = await fetch(
+        "http://192.168.0.195:8181/api/event/getAllEvent",
+        {
+          method: "GET",
+        }
+      );
+      const data = await resp.json();
+      console.log(data);
+      return data;
+    };
+    setAlldemo(await dataFetch());
+  }
+  useEffect(()=>{
+    getDetails();
+  },[])
+  console.log(alldemo)
   return (
-    <>
-      <div className="dropdown" onClick={toggleDropdown}>
-        <span style={{ fontSize: '30px' }}></span>
-      </div>
-      <div className="listData">
-        <div className="data">
-          <a href="#">Date</a>
-          <a href="#">Topic</a>
-          <a href="#">Presented By</a>
+    <div className="dropdown-container">
+      <div className="dropdown">
+        <div className="dropdown-button" onClick={toggleDropdown}>
+          <span style={{ fontSize: "30px" }}>
+            {isDropdownActive ? "-" : "+"}
+          </span>
+        </div>
+        <div className={`listData ${isDropdownActive ? "active" : ""}`}>
+          <ul className="item">
+            <li>Date</li>
+            <li>Topic</li>
+            <li>Presented By</li>
+            <li>Action</li>
+          </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
